af4ff7d6143f986ee1e4717465cc5a312c0193a2
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-04-24 10:09:48 -0400

    Add more to view_accounts

--- src/scripts/routes/accounts.py
+++ src/scripts/routes/accounts.py
@@ -1,4 +1,4 @@
-@app.route("/accounts/", methods = [ "GET", "POST"])
+@app.route("/accounts/", methods = [ "GET" ])
 def view_accounts():
 	if not validate_session(session):
 		destroy_session(session)
@@ -7,13 +7,27 @@ def view_accounts():
 	page = request.args.get("page")
 	per_page = request.args.get("per_page")
 
-	accounts, page, per_page, min_page, max_page = get_data('users', page, per_page)
+	if session.get("account_type") == "admin":
+		accounts, page, per_page, min_page, max_page = get_data('users', page, per_page)
 
-	return render_template(
-		"accounts.html",
-		accounts = accounts,
-		page = page,
-		per_page = per_page,
-		min_page = min_page,
-		max_page = max_page
-	)
+		return render_template(
+			"account.html",
+			account_type = session.get("account_type"),
+			accounts = accounts,
+			page = page,
+			per_page = per_page,
+			min_page = min_page,
+			max_page = max_page
+		)
+
+	elif session.get("account_type") == "customer":
+		account = get_query_rows(f"""
+			select * from `users`
+			where `id` = {session.get("user_id")}
+		""")
+
+		return render_template(
+			"account.html",
+			account_type = session.get("account_type"),
+			account = account
+		)
