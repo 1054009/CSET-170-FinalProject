bc4d039a68d7f6b52e1bb288bb16c3422873743a
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-04-26 14:00:15 -0400

    Add functionality to approve customer accounts

--- src/scripts/routes/accounts.py
+++ src/scripts/routes/accounts.py
@@ -45,3 +45,45 @@ def view_account_info(id):
 		account_type = session.get("account_type"),
 		account = account
 	)
+
+@app.route("/accounts/<id>", methods = [ "POST" ])
+def approve_account(id):
+	# Account does not exist
+	email_address = get_email_address(id)
+
+	if not user_exists(email_address):
+		return render_template(
+			"account_info.html",
+			message = f"User {id} does not exist",
+			id = id,
+			account_info_type = None,
+			account_type = session.get("account_type"),
+			account = None
+		)
+
+	account = get_user_info(id)[0]
+
+	# Account already approved
+	if get_account_status(id):
+		return render_template(
+			"account_info.html",
+			message = f"User {id} is already approved",
+			id = id,
+			account_info_type = get_account_type(id),
+			account_type = session.get("account_type"),
+			account = account
+		)
+
+	# Account not approved yet
+	customer_id = get_customer_id(id)
+
+	approve_customer(customer_id)
+
+	return render_template(
+		"account_info.html",
+		message = f"User {id} has been approved",
+		id = id,
+		account_info_type = get_account_type(id),
+		account_type = session.get("account_type"),
+		account = account
+	)
