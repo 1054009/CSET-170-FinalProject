381c11908085c9186e0ee99e1bf5bfdbacee2148
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-04-29 14:04:45 -0400

    Add getting description from form

--- src/scripts/routes/transactions.py
+++ src/scripts/routes/transactions.py
@@ -4,11 +4,14 @@ def view_deposit_form():
 
 @app.route("/transactions/add", methods = [ "POST" ])
 def deposit():
+	# Get data from form
 	card_number = request.form.get("card_number")
 	expiration_date = request.form.get("expiration_date")
 	ccv = request.form.get("ccv")
 	amount = request.form.get("amount")
+	description = request.form.get("description")
 
+	# Save card info
 	session["card_number"] = card_number
 	session["expiration_date"] = expiration_date
 	session["ccv"] = ccv
